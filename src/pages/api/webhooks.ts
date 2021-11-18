ec9838fb59566ef2b39790775af0cf140349edb5
hiagoLF <hiagolof@gmail.com>
2021-11-18 12:27:01 -0300

    Stripe Events

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -2,6 +2,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { Readable } from "stream";
 import Stripe from "stripe";
 import { stripe } from "../../services/stripe";
+import { saveSubscription } from "./_lib/manageSubscription";
 
 async function buffer(readalble: Readable) {
   const chunks = [];
@@ -20,11 +21,14 @@ export const config = {
 };
 
 // Set cria uma array porém nada pode estar duplicado nele
-const relevantEvents = new Set(["checkout.session.completed"]);
+const relevantEvents = new Set([
+  "checkout.session.completed",
+  // "customer.subscription.created",
+  "customer.subscription.updated",
+  "customer.subscription.deleted",
+]);
 
 async function webhooks(req: NextApiRequest, res: NextApiResponse) {
-  console.log("evento recebido");
-
   if (req.method === "POST") {
     const buf = await buffer(req);
 
@@ -44,11 +48,27 @@ async function webhooks(req: NextApiRequest, res: NextApiResponse) {
     const { type } = event;
 
     if (relevantEvents.has(type)) {
-      console.log("Evento recebido: ", event);
-
       try {
         switch (type) {
+          // case "customer.subscription.created":
+          case "checkout.subscription.updated":
+          case "customer.subscription.deleted":
+            const subscription = event.data.object as Stripe.Subscription;
+            await saveSubscription(
+              subscription.id,
+              subscription.customer.toString(),
+              false
+            );
+            break;
           case "checkout.session.completed":
+            const checkoutSession = event.data
+              .object as Stripe.Checkout.Session;
+
+            await saveSubscription(
+              checkoutSession.subscription.toString(),
+              checkoutSession.customer.toString(),
+              true
+            );
             break;
           default:
             throw new Error("Unhandled event");
